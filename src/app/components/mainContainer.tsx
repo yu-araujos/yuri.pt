b9b867fe1370eb4375c8838784e31b01d752f325
Yuri Araujo <yu.dev@icloud.com>
2024-09-21 22:24:36 +0100

    test width

--- src/app/components/mainContainer.tsx
+++ src/app/components/mainContainer.tsx
@@ -8,7 +8,7 @@ interface ContainerProps {
 
 const MainContainer: React.FC<ContainerProps> = ({ children, gradient }) => {
   return (
-    <div className={`flex flex-col justify-center items-center min-h-screen bg-gradient-to-br ${gradient} p-4`}>
+    <div className={`flex flex-col justify-center items-center min-h-screen w-full bg-gradient-to-br ${gradient} p-4`}>
       <TopBar />
       <div className="flex flex-grow justify-center items-center w-full md:w-11/12 bg-black bg-opacity-60 lg:h-[70vh]">
         {children}
